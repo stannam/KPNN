ce26454272ebe653c9d0ad361cda5bb39a911025
StanleyNam <stan.nam98@gmail.com>
2019-06-24 16:00:09 +0900

    solves #9 The additional argument 'filter' specifies which of the data-cleaning operations to be applied. Applying 'homophone removal' presupposes removing non-phonetic symbols.

--- data_cleaning.r
+++ data_cleaning.r
@@ -1,7 +1,12 @@
 if (!require(dplyr)) install.packages("dplyr")
 library(dplyr)
 
-cleanData <- function(data, entry="entry") {
+cleanData <- function(data, entry="entry", filter = "shp") {
+  # 'filter' argument 
+  # Remove various non-phonetic (S)ymbols (e.g., "그__01" becomes "그"; "우리 학교" becomes "우리학교")
+  # Remove (H)omphones
+  # Remove all words except those of specific (P)art of speech
+
   if (!any(class(data)=="data.frame")){
     stop("Please input a data.frame or tbl object.")
   }
@@ -11,11 +16,25 @@ cleanData <- function(data, entry="entry") {
   
   #if (class(data)[1]=="tbl_df") {data <- as.data.frame(data)}
   
-  data[[entry]] <- gsub("__[[:alnum:]]+", "", data[[entry]])
-  data[[entry]] <- gsub("\\(.+\\)", "", data[[entry]])
-  data[[entry]] <- gsub(" ", "", data[[entry]])
-  duplicated <- which(duplicated(data[[entry]]))
-  if(length(duplicated)>0) data <- data[-duplicated,]
+  filter <- tolower(filter)
+  
+  if(grepl("p",filter)){
+    options <- colnames(data)
+    print(paste0("Your 'corpus' has the following columns: ", paste0(options, collapse = " ")))
+    whatPOS <- readline(prompt = "Which of them represents POS? ")
+    data <- extractPOS(data, POS = whatPOS)
+  }
+  
+  if(grepl("s",filter)|grepl("h",filter)){
+    data[[entry]] <- gsub("__[[:alnum:]]+", "", data[[entry]])
+    data[[entry]] <- gsub("\\(.+\\)", "", data[[entry]])
+    data[[entry]] <- gsub(" ", "", data[[entry]])
+  }
+  
+  if(grepl("h",filter)){
+    duplicated <- which(duplicated(data[[entry]]))
+    if(length(duplicated)>0) data <- data[-duplicated,]
+  }
   
   return(data)
 }
